34c06b3d238c6d767ec53d3249b9bdca8fbb9da1
deshondixon <deshondixon@gmail.com>
2022-10-18 18:08:26 -0700

    got lost following lab

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
 'use strict';
 
-console.log("duck,duck,goose!");
+let indexArray = [];
 
 let myContainer = document.querySelector('section');
 let resultBtn = document.querySelector('section + div');
 let results = document.querySelector('ul');
-
 let product1 = document.querySelector('section img:first-child');
 let product2 = document.querySelector('section img:nth-child(2)');
-
 let howManyTimesUserHasVoted = 0;
-let maxNumberOfVotes = 15;
+let maxNumberOfVotes = 17;
+
 
 function OddProducts(name, fileExtension = 'jpg') {
   this.name = name;
@@ -41,22 +40,22 @@ let waterCan = new OddProducts('water-can');
 let wine = new OddProducts('wine-glass');
 
 let allOddProducts = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogDuck, dragon, pen, petSweep, scissors, shark, sweep, spaceKid, unicorn, waterCan, wine];
-console.log(allOddProducts);
 
 function selectRandomOddProducts() {
-
   return Math.floor(Math.random() * allOddProducts.length);
 }
 
 function renderOddProducts() {
-  let odd1 = selectRandomOddProducts();
-  let odd2 = selectRandomOddProducts();
-  console.log(odd1, odd2);
 
-  while (odd1 === odd2) {
-    odd2 = selectRandomOddProducts();
-    console.log(odd1, odd2);
+  while (indexArray.length < 19) {
+    let ranProd = selectRandomOddProducts ();
+    if (!indexArray.includes(ranProd)) {
+      indexArray.push(ranProd);
+    }
   }
+  console.log(indexArray);
+  let odd1 = indexArray.shift();
+  let odd2 = indexArray.shift();
 
   product1.src = allOddProducts[odd1].src;
   product1.alt = allOddProducts[odd1].name;
@@ -67,25 +66,19 @@ function renderOddProducts() {
   allOddProducts[odd2].views++;
 }
 
+
 function renderResults() {
-  for (let i= 0; i < allOddProducts.length; i++) {
-    let li = document.createElement('li');
-    li.textContent = `${allOddProducts[i].name} had ${allOddProducts[i].views} views and ${allOddProducts[i].score} votes`;
-    results.appendChild(li);
-  }
 }
 
 function handleClick(event) {
   if (event.target === myContainer) {
     alert('Select product')
   }
-  console.log(event.target.alt);
   howManyTimesUserHasVoted++;
   let clickedOddProducts = event.target.alt;
 
   for (let i = 0; i < allOddProducts.length; i++) {
     if (clickedOddProducts === allOddProducts[i].name) {
-      console.log(allOddProducts[i]);
       allOddProducts[i].score++;
       break;
     }
@@ -93,10 +86,66 @@ function handleClick(event) {
   if (howManyTimesUserHasVoted === maxNumberOfVotes) {
     myContainer.removeEventListener('click', handleClick);
     resultBtn.className = 'clicks-allowed';
-    resultBtn.addEventListener('click', renderResults)
+    resultBtn.addEventListener('click', renderChart)
+    renderChart();
   } else {
     renderOddProducts();
   }
+}
+
+function renderChart() {
+  let OddProductsNames = [];
+  let OddProductsViews = [];
+  let OddProductsScore = [];
+  for (let i = 0; i < allOddProducts.length; i++) {
+    OddProductsNames.push(allOddProducts[i].name);
+    OddProductsViews.push(allOddProducts[i].views);
+    OddProductsScore.push(allOddProducts[i].score);
+  }
+  const data = {
+    labels: OddProductsNames,
+    datasets: [
+      {
+        label: '# of views',
+        data: OddProductsViews,
+        backgroundColor: [
+          'yellow'
+        ],
+        borderColor: [
+          'orange'
+        ],
+        borderWidth: 7
+      },
+      {
+        label: '# of votes',
+        data: OddProductsScore,
+        backgroundColor: [
+          'red'
+        ],
+        borderColor: [
+          'blue'
+        ],
+        borderWidth: 4
+      }
+    ]
+  };
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+      indexAxis:'y',
+      scales: {
+        y: {
+          stacked:true
+        },
+      }
+    }
+  };
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
+
 }
 
 myContainer.addEventListener('click', handleClick);
